f5746061df578f838bb220df91441af8fd55912b
morzeszko <55149644+morzeszko@users.noreply.github.com>
2019-09-11 19:50:14 +0200

    wprawki

---
+++ Wprawki/Wprawki/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wprawki
+{
+    class Program
+    {
+        /*static void Main1(string[] args)
+        {
+            int x = int.MaxValue;
+            int y = 2;
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+            static void Main2(string[] args)
+            {
+            double x = 0.1;
+            double y = 0.2;
+            double z = 0.3;
+            Console.WriteLine(x+y==z);} //  false */
+
+        static void Main3(string[] args)
+        {
+            decimal x = 0.1m; //m- money czyli 2 mca po przecinku
+                              //float y = 0.1f; //literał typu float- domyślnie 0.1 jest typu double
+
+            decimal suma = 0; // automatyczna kompresja 0 ---> 0.0
+
+            for (int i = 1; i <= 1000000; i++)
+            {
+                suma = suma + x;
+            }
+            Console.WriteLine(suma);
+        }
+        static void Main4(string[] args)
+        {
+            char znak = 'a';
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine((char)(znak + i));
+            }
+            string napis = "a";
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(napis + i);
+            }
+        }
+        static void Main5(string[] args)
+        {
+            string napis = "ala ma kota";
+            for (int i = napis.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(napis[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int x=-3, y=-15;
+            int z = 0;
+            int u = x;
+            do
+            {
+                z += y; //z=z+y
+                u--;
+            } while (u!=0);
+            Console.WriteLine(z);
+        }
+    }
+}
+
---
+++ Wprawki/Wprawki/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Wprawki")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wprawki")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("3f1d0853-b29f-4ca2-98ba-0c5ab2a3333b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
